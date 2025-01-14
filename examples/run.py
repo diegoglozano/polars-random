cf812d6ac27ed73101680ce57d28241be0cc3ffc
Diego García Lozano <38294364+diegoglozano@users.noreply.github.com>
2025-01-14 04:31:09 +0000

    [feat] Change logic. Now we extend dataframe namespace

--- examples/run.py
+++ examples/run.py
@@ -1,19 +1,16 @@
 import polars as pl
 import polars_random
-import os
-
-os.environ["POLARS_VERBOSE"] = "1"
 
 df = pl.DataFrame(
     {
         "a": [0.2, 0.5, 0.7, 0.8, 0.9],
     }
-).with_columns(
-    pl.col("a").random.rand().alias("rand"),
-    pl.col("a").random.rand(low=10, high=11, seed=42).alias("rand_10_11"),
-    pl.col("a").random.normal().alias("normal"),
-    pl.col("a").random.normal(seed=42).alias("normal_seed1"),
-    pl.col("a").random.normal(seed=42).alias("normal_seed2"),
-    pl.col("a").random.binomial(n=10, p=.3).alias("binomial"),
 )
-print(df)
+
+print(
+    df
+    .random.rand(seed=42)
+    .random.normal(seed=42, name="normal_seed_1")
+    .random.normal(seed=42, name="normal_seed_2")
+    .random.binomial(24, .5, seed=42)
+)
