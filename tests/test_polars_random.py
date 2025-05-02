ebc649781b66c046353549efcd467087553b50f8
Juraj Mičko <juraj.micko@quantco.com>
2025-05-02 19:03:46 +0200

    support LazyFrame and streaming in tests

--- tests/test_polars_random.py
+++ tests/test_polars_random.py
@@ -12,19 +12,31 @@ import polars_random  # noqa
         ("binomial", dict(n=10, p=0.5), 5.0),
     ],
 )
-def test_all(func_name: str, params: dict[str, float], target: float) -> None:
+@pytest.mark.parametrize(
+    ("lazy", "streaming"),
+    [
+        (False, False),
+    ],
+)
+def test_all(
+    lazy: bool, streaming: bool, func_name: str, params: dict[str, float], target: float
+) -> None:
     # Initialize the dataframe
     df: pl.DataFrame | pl.LazyFrame = pl.DataFrame(
         {
             "a": range(1_000_000),
         }
     )
+    if lazy:
+        df = df.lazy()
 
     # Add the random column
     df = getattr(df.random, func_name)(**params, name="rand")  # type: ignore
 
     # Calculate the mean
     df_mean = df.select(pl.col("rand").mean())
+    if lazy:
+        df_mean = df_mean.collect(streaming=streaming)
     mean = df_mean.item()
 
     assert abs(mean - target) < 0.01
